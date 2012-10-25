ebbac8bc5ac9d1d88e51ce195cd90d5618735cf4
Ben Firner <bfirner@eden.rutgers.edu>
2012-10-25 13:03:24 -0400

    Adding license.

--- owl-proximity.rb
+++ owl-proximity.rb
@@ -1,3 +1,26 @@
+#Copyright (c) 2012 Bernhard Firner
+#All rights reserved.
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+#or visit http://www.gnu.org/licenses/gpl-2.0.html
+
+
+#@file owl-proximity.rb
+#Detects proximity between transmitters and receivers.
+#Uses RSS values from the fingerprint solver and a user-provided RSS threshold
+
 #Require rubygems for old (pre 1.9 versions of Ruby and Debian-based systems)
 require 'rubygems'
 require 'client_world_connection.rb'
@@ -143,3 +166,4 @@ while (cwm.connected and not sensor_request.isComplete() and not rss_request.isC
 end
 
 exit
+
